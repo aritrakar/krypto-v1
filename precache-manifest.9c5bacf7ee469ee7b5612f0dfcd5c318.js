3001b36e019c798fae4096e18519d7117966b096
Aritra Kar <aritrakar2002@gmail.com>
2020-08-30 22:28:02 +0530

    Updates

---
+++ precache-manifest.9c5bacf7ee469ee7b5612f0dfcd5c318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b28603d95d70db539f4a70fc7291e7",
+    "url": "/krypto-v1/index.html"
+  },
+  {
+    "revision": "0f4fdf1ad41ea99fe54a",
+    "url": "/krypto-v1/static/css/main.b6c439d5.chunk.css"
+  },
+  {
+    "revision": "49203060aecde5379c97",
+    "url": "/krypto-v1/static/js/2.8e5d2275.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/krypto-v1/static/js/2.8e5d2275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4fdf1ad41ea99fe54a",
+    "url": "/krypto-v1/static/js/main.024e512a.chunk.js"
+  },
+  {
+    "revision": "537b05e688051669c4de",
+    "url": "/krypto-v1/static/js/runtime-main.edb7b434.js"
+  }
+]);
